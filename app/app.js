273786bb566ff4b1685beca16b3650749769df40
michaelhernandez <michaeladrian.hernandez.rala@gmail.com>
2022-11-25 23:54:23 +0100

    .env config

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ const OpenApiValidator = require('express-openapi-validator');
 const swaggerUi = require('swagger-ui-express');
 const logger = require('./utils/logger');
 
-const config = require('./config/config');
+require('dotenv').config();
 
 const swaggerDocument = YAML.load(fs.readFileSync(path.join(__dirname, 'swagger', 'swagger.yaml')), 'utf-8');
 
@@ -26,7 +26,7 @@ app.use(
   }),
 );
 
-const PORT = config.PORT || 3606;
+const PORT = process.env.PORT || 3606;
 
 app.listen(PORT, () => logger.info({ message: `[TYPEBOT] Server listening on port ${PORT}` }));
 
--- app/lib/db.js
+++ app/lib/db.js
@@ -1,9 +1,13 @@
 const Sequelize = require('sequelize');
 
 const {
-  DATABASE, USERNAME, PASSWORD, dbConfig,
-} = process.env.sequelize;
+  DATABASE, USERNAME, PASSWORD,
+} = process.env;
 
-const db = new Sequelize(DATABASE, USERNAME, PASSWORD, dbConfig);
+const db = new Sequelize(DATABASE, USERNAME, PASSWORD, {
+  host: 'localhost',
+  dialect: 'postgres',
+
+});
 
 module.exports = db;
