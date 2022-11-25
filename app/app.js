90c69996193138f31c3a9db0c963d1f30e5cb0c4
michaelhernandez <michaeladrian.hernandez.rala@gmail.com>
2022-11-25 23:24:19 +0100

    fix PORT

--- app/app.js
+++ app/app.js
@@ -26,7 +26,7 @@ app.use(
   }),
 );
 
-const PORT = process.env.PORT || 3606;
+const PORT = process.env.PORT || 3610;
 
 app.listen(PORT, () => logger.info({ message: `[TYPEBOT] Server listening on port ${PORT}` }));
 
